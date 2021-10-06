13178ff687cec1ad23613b126f381887bb22fbcb
WeimarC <89791568+WeimarC@users.noreply.github.com>
2021-10-06 16:04:09 -0300

    Parte Final CSS

--- src/components/Imagen.jsx
+++ src/components/Imagen.jsx
@@ -1,3 +1,5 @@
+import "./styles/Main.css";
+
 const alturas = [260, 153, 300];
 const imgs = ["./img/260.jpg", "./img/153.jpg", "./img/300.jpg", "./img/400.jpg", "./img/230.jpg"];
 
@@ -6,7 +8,7 @@ const Imagen = () => {
   const urlImg = imgs[indexRnd];
   const altura = alturas[indexRnd];
   return (
-    <img
+    <img className="Img"
       src={urlImg} alt="texto" width="220px" height={altura} /> );
   };
 
